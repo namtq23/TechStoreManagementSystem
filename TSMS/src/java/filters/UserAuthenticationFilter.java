e8fe75fae77fd3a854d8b4d7835e0ee71125b068
Phuong <ndpp.work@gmail.com>
2025-06-04 14:33:46 +0700

    phuongndp-authen
    admin authen
    user authen

---
+++ TSMS/src/java/filters/UserAuthenticationFilter.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package filters;
+
+/**
+ *
+ * @author Astersa
+ */
+import java.io.IOException;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebFilter(filterName = "AuthenticationFilter", urlPatterns = {"/bm-products", "/bm-overview", "/bm-staff", "/bm-customer"})
+public class UserAuthenticationFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        if (httpRequest.getSession().getAttribute("userId") == null) {
+            httpResponse.sendRedirect(httpRequest.getContextPath() + "/login");
+        } else {
+            chain.doFilter(request, response); 
+        }
+        
+        
+    }
+
+    @Override
+    public void destroy() {
+        // Cleanup logic (if needed)
+    }
+}
